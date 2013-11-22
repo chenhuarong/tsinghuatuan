56232e66fdfd620cd36e7028fcd938a8db5033ea
Huarong Chen <chenhuarongzp@gmail.com>
2013-11-22 23:28:50 +0800

    for a better understanding of app.conf

--- app.py
+++ app.py
@@ -16,15 +16,20 @@ django_WSGI = WSGIHandler()
 from queryhandler import handle_weixin_request
 
 def app(environ, start_response):
-    if environ['PATH_INFO'] == LUCKY_URL:
+    """if environ['PATH_INFO'] == LUCKY_URL:
         result = handle_weixin_request(environ)
         status = '200 OK'
         headers = [('Content-type', 'text/html')]
         start_response(status, headers)
         return [result.encode('utf8')]
     else:
-        status = '200 OK'
-        headers = [('Content-type', 'text/html')]
-        start_response(status, headers)
-        return [environ['PATH_INFO']]
-        #return django_WSGI.__call__(environ, start_response)
+        #status = '200 OK'
+        #headers = [('Content-type', 'text/html')]
+        #start_response(status, headers)
+        #return [environ['PATH_INFO']]
+        return django_WSGI.__call__(environ, start_response)"""
+    status = '200 OK'
+    headers = [('Content-type', 'text/html')]
+    start_response(status, headers)
+    return [environ['PATH_INFO']]
+
