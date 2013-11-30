8e16376bd2b5190ca0c6730eac0751828d55f2ca
Huarong Chen <chenhuarongzp@gmail.com>
2013-11-30 09:34:46 +0800

    Url refector.

--- urlhandler/adminpage/views.py
+++ urlhandler/adminpage/views.py
@@ -17,8 +17,15 @@ import datetime
 #import database
 from urlhandler.models import Activity
 
-
 def home(request):
+    return HttpResponse('Hi')
+
+def activity_detail(request, actid):
+    return HttpResponse(str(actid))
+
+
+def activity_list(request):
     activities = Activity.objects.all()
-    return render_to_response('activity_home.html', locals())
-    #return render_to_response('activity_management.html', locals())
+    return render_to_response('activity_list.html', locals())
+    #return render_to_response('activity_detail.html', locals())
+
