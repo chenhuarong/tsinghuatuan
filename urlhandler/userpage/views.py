450905362acd5e92db2a25da12bb67caf90507fb
yl-1993 <yl93528@gmail.com>
2013-12-01 23:56:38 +0800

    complete initial mobile info interaction

--- urlhandler/userpage/views.py
+++ urlhandler/userpage/views.py
@@ -3,8 +3,10 @@
 from django.http import HttpResponse, Http404
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from urlhandler.models import User
+from urlhandler.models import User, Activity
+from urlhandler.settings import STATIC_URL
 import urllib, urllib2
+from django.utils import timezone
 
 def home(request):
     return render_to_response('mobile_base.html')
@@ -79,3 +81,36 @@ def validate_post(request):
             except:
                 return HttpResponse('Error')
     return HttpResponse(validate_result)
+
+###################### Activity Detail ######################
+
+def details_view(request):
+    requestdata = request.GET
+    if (not requestdata) or (not 'activityid' in requestdata):
+        raise Http404
+    activity = Activity.objects.filter(id=requestdata.get('activityid', ''))
+    if not activity.exists():
+        raise Http404  #current activity is invalid
+    act_name = activity[0].name
+    act_key = activity[0].key
+    act_place = activity[0].place
+    act_bookstart = activity[0].book_start
+    act_bookend = activity[0].book_end
+    act_begintime = activity[0].start_time
+    act_endtime = activity[0].end_time
+    act_totaltickets = activity[0].total_tickets
+    act_perorder = activity[0].max_tickets_per_order
+    act_text = activity[0].description
+    act_photo = STATIC_URL + "img/mlhk.png"
+    cur_time = timezone.now() # use the setting UTC
+    if act_bookstart <= cur_time <= act_bookend:
+        act_status = u'订票正在进行中，请回复代码：“%s+订票张数”或点击菜单栏中“%s”进行订票'%(act_key,act_name)
+    elif cur_time < act_bookstart:
+        act_status = u'订票尚未开始，订票时间为:%s-%s'%(act_bookstart, act_bookend)
+    else:
+        act_status = u'活动订票已结束，可回复代码：%s查询订票结果'%(act_key)
+    variables=RequestContext(request,{'act_name':act_name,'activity_text':act_text, 'activity_photo':act_photo,
+                                      'act_bookstart':act_bookstart,'act_bookend':act_bookend,'act_begintime':act_begintime,
+                                      'act_endtime':act_endtime,'act_totaltickets':act_totaltickets,'act_perorder':act_perorder,
+                                      'act_place':act_place, 'act_status':act_status})
+    return render_to_response('activitydetails.html', variables)
