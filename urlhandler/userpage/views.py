4e8e01dd87ef826f9ef2d927cf60375523e285b0
yl-1993 <yl93528@gmail.com>
2013-12-04 02:40:25 +0800

    pic url

--- urlhandler/userpage/views.py
+++ urlhandler/userpage/views.py
@@ -106,7 +106,7 @@ def details_view(request):
     if len(act_text) > MAX_LEN:
         act_text_status = 1
         act_abstract = act_text[0:MAX_LEN]+u'...'
-    act_photo = STATIC_URL + "img/mlhk.png"
+    act_photo = activity[0].pic_url
     cur_time = timezone.now() # use the setting UTC
     act_seconds = 0
     if act_bookstart <= cur_time <= act_bookend:
