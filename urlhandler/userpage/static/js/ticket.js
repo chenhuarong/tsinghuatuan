7d54a0086aa51dced705aa0572775a108c802983
yl-1993 <yl93528@gmail.com>
2013-12-04 13:44:32 +0800

    page modify, disable share button

---
+++ urlhandler/userpage/static/js/ticket.js
@@ -0,0 +1,10 @@
+/**
+ * Created with PyCharm.
+ * User: LY
+ * Date: 13-12-4
+ * Time: 下午1:37
+ * To change this template use File | Settings | File Templates.
+ */
+
+//disable the sharing button
+window.setupWeixin({'optionMenu':false, 'toolbar':false});
